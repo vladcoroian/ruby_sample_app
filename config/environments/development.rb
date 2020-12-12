a9f3951c9ea8aaec96e28d9d3796a56ea3fca856
Vlad Coroian <vladcoroian2001@gmail.com>
2020-12-12 19:45:50 +0000

    Implement basic login - problem with dropdown & delete logout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
@@ -51,5 +51,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << "50d98406c31649dcac593bad4d59f193.vfs.cloud9.us-east-2.amazonaws.com"
+  config.hosts << "0110939bbde542f8b2e9a6aac558ecde.vfs.cloud9.us-east-2.amazonaws.com"
 end
