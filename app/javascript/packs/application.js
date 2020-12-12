f37ecfa0be2a2b5006a9601c68a6069038dd49a4
Vlad Coroian <vladcoroian2001@gmail.com>
2020-12-12 22:35:56 +0000

    Try to fix herkou

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,10 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+import "bootstrap"
